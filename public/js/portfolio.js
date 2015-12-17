2ee8cd73004af59039209bada0a3a969894e2ae4
pytong <pytong07@gmail.com>
2015-12-17 18:44:30 +0000

    Added project overlay item animation

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,8 +1,21 @@
+"use strict";
+
 $(function() {
-  $(".project").mouseenter(function() {
-    $(".project-overlay").show();
+  $(".project").mouseenter(function(event) {
+    let projectId = $(event.target).parent().data("id"),
+      projectEl = $(`.${projectId}`),
+      iconEls = projectEl.find(".icons");
+
+    projectEl.find(".project-overlay").fadeIn(500);
+    iconEls.animate({top: "-=110px"});
   });
-  $(".project").mouseleave(function() {
-    $(".project-overlay").hide();
+
+  $(".project").mouseleave(function(event) {
+    let projectId = $(event.target).parent().data("id"),
+      projectEl = $(`.${projectId}`),
+      iconEls = projectEl.find(".icons");
+
+    projectEl.find(".project-overlay").hide();
+    iconEls.css("top", "205px");
   });
 });
