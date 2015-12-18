ae949b3895c6f8840e3844b388e8049e3acdf29c
pytong <pytong07@gmail.com>
2015-12-18 02:10:36 +0000

    Fixed project hover animation

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -8,8 +8,8 @@ $(function() {
       iconsEl = projectEl.find(".icons");
 
     projectEl.find(".project-overlay").fadeIn(500);
-    titleEl.animate({"top": "50px"});
-    iconsEl.animate({"top": "80px"});
+    titleEl.animate({"top": "60px"}, 500);
+    iconsEl.animate({"top": "80px"}, 650);
   });
 
   $(".project").mouseleave(function(event) {
@@ -18,12 +18,12 @@ $(function() {
       titleEl = projectEl.find(".title"),
       iconsEl = projectEl.find(".icons");
 
-    projectEl.stop();
-    iconsEl.stop();
+    projectEl.stop(true, true);
+    titleEl.stop(true, true);
+    iconsEl.stop(true, true);
 
     titleEl.css("top", "-30px");
     iconsEl.css("top", "205px");
     projectEl.find(".project-overlay").css("display", "none");
-
   });
 });
