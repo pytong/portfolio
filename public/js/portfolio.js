11bb162619bd21c271aed94be1912c2a8aef20b7
pytong <pytong07@gmail.com>
2015-12-18 01:17:57 +0000

    Added animation for project title

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -4,18 +4,26 @@ $(function() {
   $(".project").mouseenter(function(event) {
     let projectId = $(event.target).parent().data("id"),
       projectEl = $(`.${projectId}`),
-      iconEls = projectEl.find(".icons");
+      titleEl = projectEl.find(".title"),
+      iconsEl = projectEl.find(".icons");
 
     projectEl.find(".project-overlay").fadeIn(500);
-    iconEls.animate({top: "-=110px"});
+    titleEl.animate({"top": "50px"});
+    iconsEl.animate({"top": "80px"});
   });
 
   $(".project").mouseleave(function(event) {
     let projectId = $(event.target).parent().data("id"),
       projectEl = $(`.${projectId}`),
-      iconEls = projectEl.find(".icons");
+      titleEl = projectEl.find(".title"),
+      iconsEl = projectEl.find(".icons");
 
-    projectEl.find(".project-overlay").hide();
-    iconEls.css("top", "205px");
+    projectEl.stop();
+    iconsEl.stop();
+
+    titleEl.css("top", "-30px");
+    iconsEl.css("top", "205px");
+    projectEl.find(".project-overlay").css("display", "none");
+
   });
 });
